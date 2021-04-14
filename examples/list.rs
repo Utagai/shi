940c395484a5218352da32144dce9705810d7c9b
may h <mehrabhoque@gmail.com>
2021-04-14 13:11:06 -0400

    Add a simple list example

---
+++ examples/list.rs
@@ -0,0 +1,48 @@
+use shi::shell::Shell;
+use shi::{cmd, parent};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let lst: Vec<String> = Vec::new();
+
+    let mut shell = Shell::new_with_state("| ", lst);
+
+    shell.set_and_load_history_file("readline_history.txt")?;
+    shell.register(cmd!("pop", |the_lst: &mut Vec<String>, _| {
+        let _ = the_lst.pop();
+        Ok(String::from("popped last item"))
+    }))?;
+    shell.register(cmd!("list", |the_lst: &mut Vec<String>, _| {
+        Ok(format!(
+            "Current: [{}]",
+            the_lst
+                .iter()
+                .map(|f| format!("{:?}", f))
+                .collect::<Vec<String>>()
+                .join(", ")
+        ))
+    }))?;
+    shell.register(parent!(
+        "add",
+        cmd!("title", |the_lst: &mut Vec<String>, _| {
+            the_lst.push("title".to_owned());
+            Ok(String::from("Added 'title'"))
+        }),
+        parent!(
+            "isbn",
+            cmd!("eu", |the_lst: &mut Vec<String>, _| {
+                the_lst.push("eu".to_owned());
+                Ok(String::from("Added 'eu'"))
+            }),
+            cmd!("us", |the_lst: &mut Vec<String>, _| {
+                the_lst.push("us".to_owned());
+                Ok(String::from("Added 'us'"))
+            }),
+        ),
+    ))?;
+
+    shell.run()?;
+
+    Ok(())
+}
